9a6bec152556f9a657f230085a3288e6d0a33596
bluicezhen <bluicezhen@tencent.com>
2022-06-22 20:37:45 +0800

    init

---
+++ README.md
@@ -0,0 +1,42 @@
+# excel2echart
+
+A python tool for transform excel file to eChart HTML.
+
+## 1. Installation
+
+```bash
+git clone git@github.com:bluicezhen/excel2ecahrt.git
+cd excel2echart
+pipenv install
+mkdir bin
+cat > ./bin/excel2ecahrt.sh <<- EOF
+#!/bin/bash
+filename="\$1"
+cd "$(pwd)"
+pipenv shell
+python excel2echart.py "\$filename"
+EOF
+chmod +x ./bin/excel2ecahrt.sh
+echo "export PATH="$(pwd)/bin:$PATH" >> ~/.bash_profile
+```
+
+## 2. Use
+
+```bash
+excel2ecahrt.sh some.xlsx
+```
+
+## 3. Excel example
+
+| date       | bbl   | bmt   | gy    | xq   | ldjj  | mn    | sllh  | wd    |
+| ---------- | ----- | ----- | ----- | ---- | ----- | ----- | ----- | ----- |
+| 2022-05-26 |       | 100.5 | 118.2 |      | 109.3 | 76.3  |       | 57    |
+| 2022-05-30 |       | 108.5 |       |      |       |       |       |       |
+| 2022-06-06 |       | 166.8 | 153.7 | 83.1 | 145.7 | 131.6 | 86.6  | 119.1 |
+| 2022-06-13 |       |       | 162.3 |      |       |       |       |       |
+| 2022-06-14 |       |       |       |      |       |       | 88.7  |       |
+| 2022-06-15 |       |       |       |      |       |       | 90.5  |       |
+| 2022-06-16 |       |       |       |      |       |       | 115.9 |       |
+| 2022-06-19 |       |       |       |      |       |       | 117.1 |       |
+| 2022-06-21 | 142.3 |       |       |      |       |       | 117.7 |       |
+| 2022-06-22 | 146.4 |       |       |      |       |       | 117.7 |       |
---
+++ excel2echart.py
@@ -0,0 +1,63 @@
+import json
+import re
+import sys
+from openpyxl import load_workbook
+
+wb = load_workbook(
+    filename=sys.argv[1], read_only=True)
+ws = wb[wb.sheetnames[0]]
+
+option = {
+    'xAxis': {
+        'type': 'time'
+    },
+    'yAxis': {
+        'type': 'value'
+    },
+    'tooltip': {
+        'trigger': 'axis',
+        'order': 'valueDesc'
+    },
+    'legend': {
+        'data': []
+    },
+    'series': []
+}
+i = 0
+for row in ws.rows:
+    j = 0
+    if i == 0:
+        for cell in row:
+            if j > 0:
+                option['legend']['data'].append(cell.value)
+                option['series'].append({
+                    'name': cell.value,
+                    'type': 'line',
+                    'smooth': True,
+                    'data': []
+                })
+            j += 1
+    else:
+        for cell in row:
+            if j == 0:
+                time_name = cell.value.strftime('%Y-%m-%d')
+            else:
+                if cell.value:
+                    option['series'][j - 1]['data'] \
+                        .append([time_name, cell.value])
+                if cell.value is None and (i + 1) == ws.max_row:
+                    option['series'][j - 1]['data'] \
+                        .append([time_name, max([k[1] for k in option['series'][j - 1]['data']])])
+            j += 1
+    i += 1
+
+wb.close()
+
+json_data = json.dumps(option, ensure_ascii=False, indent=None)
+
+html_template = open('./chart.html.template', 'rt')
+html_template_str = html_template.read()
+html_template.close()
+
+html_str = re.sub(r'\{\{option\}\}', json_data, html_template_str)
+print(html_str)
